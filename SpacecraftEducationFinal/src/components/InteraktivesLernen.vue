e48a59bb61c375fc1b84bb3ea2dd033e883e85cb
Ltrou <ltroudereinzigwahre1@gmail.com>
2024-07-30 13:11:28 +0200

    bisschen rumgespielt an IntroScreen

--- SpacecraftEducationFinal/src/components/InteraktivesLernen.vue
+++ SpacecraftEducationFinal/src/components/InteraktivesLernen.vue
@@ -1,96 +1,91 @@
 <template>
-        <div class="background">
-                <h1>INTERAKTIVES LERNEN</h1>
-                <div class="wrapper">
-                        <div class="outer-flexbox">
-                                <div class="column inner-flexbox-left">
-                                        <p class="text item item-1">
-                                                Der Satellitenbaukasten bietet
-                                                eine
-                                                einzigartige
-                                                Möglichkeit, die Form und
-                                                Funktion von
-                                                Satelliten durch hands-on
-                                                Erfahrung zu
-                                                erlernen.
-                                                Jedes Modell enthält Bauteile,
-                                                die
-                                                vereinfacht
-                                                realen Satellitenkomponenten
-                                                entsprechen.
-                                        </p>
+      <div class="background">
+            <h1>INTERAKTIVES LERNEN</h1>
+            <div class="wrapper">
+                  <div class="outer-flexbox">
+                        <div class="column inner-flexbox-left">
+                              <p class="text item item-1">
+                                    Der Satellitenbaukasten bietet eine
+                                    einzigartige Möglichkeit, die Form und
+                                    Funktion von Satelliten durch hands-on
+                                    Erfahrung zu erlernen. Jedes Modell enthält
+                                    Bauteile, die vereinfacht realen
+                                    Satellitenkomponenten entsprechen.
+                              </p>
 
-                                        <img class="item item-2"
-                                                src="../assets/Interaktives_Lernen_Personen_Bau.png"
-                                                alt="BeispielLukas" />
-                                </div>
-
-
-                                <div class="column inner-flexbox-right">
-                                        <img class="item item-3"
-                                                src="../assets/Satellit_Explo_alpha.png"
-                                                alt="SatellitExplosion" />
-                                </div>
-
+                              <img
+                                    class="item item-2"
+                                    src="../assets/Interaktives_Lernen_Personen_Bau.png"
+                                    alt="BeispielLukas"
+                              />
                         </div>
-                </div>
-        </div>
+
+                        <div class="column inner-flexbox-right">
+                              <img
+                                    class="item item-3"
+                                    src="../assets/Satellit_Explo_alpha.png"
+                                    alt="SatellitExplosion"
+                              />
+                        </div>
+                  </div>
+            </div>
+      </div>
 </template>
 
 <script>
 export default {
-        name: 'InteraktivesLernen',
-}
+      name: "InteraktivesLernen",
+};
 </script>
 
 <style scoped>
 h1 {
-        flex: 1 1 100%;
-        font-size: var(--font-size-head);
-        margin: 0px;
+      flex: 1 1 100%;
+      font-size: var(--font-size-head);
+      margin: 0px 0px 0px 5%;
 }
 
 .background {
-        padding-top: 30px;
-        min-height: var(--min-height-segment);
-        display: flex;
-        justify-content: center;
-        flex-wrap: wrap;
+      padding-top: 30px;
+      min-height: var(--min-height-segment);
+      display: flex;
+      justify-content: center;
+      flex-wrap: wrap;
 }
 
-.wrapper{
-        border: 1px solid black;
-        max-width: var(--max-width-page);
-        width: 100%;
+.wrapper {
+      border: 1px solid rgb(21, 255, 0);
+      max-width: var(--max-width-page);
+      width: 100%;
 }
 .outer-flexbox {
-        display: flex;
-        justify-content: center;
-        width: 100%;
-        gap: 3%;
-        padding-bottom: max(3%, 50px);
-        padding-left: 3%;
+      border: 1px solid rgb(255, 0, 0);
 
+      display: flex;
+      justify-content: center;
+      width: 100%;
+      gap: 3%;
+      padding-bottom: max(3%, 50px);
+      padding-left: 3%;
 }
 
 .inner-flexbox-left {
-        border: 1px solid black;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        /* width: 100%; */
+      border: 1px solid rgb(255, 255, 255);
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      /* width: 100%; */
 }
 
 .inner-flexbox-right {
-        border: 1px solid black;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        /* width: 100%; */
+      border: 1px solid black;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      /* width: 100%; */
 }
-
 
 /* .item{
         flex-basis: 50%;
@@ -98,35 +93,35 @@ h1 {
 } */
 /* Macht text Sinn hier? */
 .text {
-        font-size: var(--font-size-normal);
-        max-width: 700px;
-        /* maybe variable erstellen*/
+      font-size: var(--font-size-normal);
+      max-width: 700px;
+      /* maybe variable erstellen*/
 }
 
 img {
-        width: 60%;
-        min-width: 200px;
-        /* maybe variable erstellen*/
-        max-width: 600px;
-        /* irwie steuer ich gerade die größe vom Farbcontainer damit*/
+      width: 60%;
+      min-width: 200px;
+      /* maybe variable erstellen*/
+      max-width: 600px;
+      /* irwie steuer ich gerade die größe vom Farbcontainer damit*/
 
-        /* maybe variable erstellen*/
-        border-radius: var(--border-radius-photo);
+      /* maybe variable erstellen*/
+      border-radius: var(--border-radius-photo);
 }
 
 /* ---------------------------Animation--------------------------- */
 
 @keyframes fadeInLeft {
-        0% {
-                opacity: 0;
-        }
+      0% {
+            opacity: 0;
+      }
 
-        100% {
-                opacity: 1;
-        }
+      100% {
+            opacity: 1;
+      }
 }
 
 h1 {
-        animation: fadeInLeft 2s ease-in-out forwards;
+      animation: fadeInLeft 2s ease-in-out forwards;
 }
 </style>
--- SpacecraftEducationFinal/src/components/IntroScreen.vue
+++ SpacecraftEducationFinal/src/components/IntroScreen.vue
@@ -1,124 +1,125 @@
 <template>
-        <div class="background">
-                <div class="wrapper">
-                        <h1>EIN STÜCK WELTRAUM IN DEINEN HÄNDEN</h1>
-                        <div class="flexbox flexbox-1">
-                                <img src="../assets/Intro_Mädchen_Bau.png"
-                                        alt="Schülerin baut Satellit" />
-                                <p class="text text-1">
-                                        Willkommen in der faszinierenden Welt
-                                        der
-                                        Raumfahrt mit
-                                        unserem Satellitenbaukasten! Unser
-                                        innovatives
-                                        Produkt ermöglicht es spielend zu
-                                        lernen, was
-                                        Satelliten
-                                        sind, wie sie aufgebaut sind, und welche
-                                        Aufgaben sie im
-                                        Weltraum übernehmen.
-                                </p>
-                                <img src="../assets/Intro_SatellitenBauKasten.png"
-                                        alt="Hilfe beim Satelliten bauen" />
-                        </div>
-                        <div class="flexbox flexbox-2">
-                                <img src="../assets/Intro_Stefan_Bau.png"
-                                        alt="Zwei Satellit-Baukasten" />
-                                <p class="text text-2">
-                                        Der Satellitenbaukasten besteht aus
-                                        3D-gedruckten
-                                        Satellitenmodellen in Originalgröße, die
-                                        aus
-                                        verschiedenen Satellitenkomponenten
-                                        bestehen und
-                                        selbstständig zusammengebaut werden
-                                        können.
-                                </p>
-                                <img src="../assets/Intro_Junge_Bau.png"
-                                        alt="Schüler baut Satelliten" />
-                        </div>
-                </div>
-        </div>
+      <div class="background">
+            <div class="wrapper">
+                  <h1>EIN STÜCK WELTRAUM IN DEINEN HÄNDEN</h1>
+                  <div class="flexbox flexbox-1">
+                        <img
+                              src="../assets/Intro_Mädchen_Bau.png"
+                              alt="Schülerin baut Satellit"
+                        />
+                        <p class="text text-1">
+                              Willkommen in der faszinierenden Welt der
+                              Raumfahrt mit unserem Satellitenbaukasten! Unser
+                              innovatives Produkt ermöglicht es spielend zu
+                              lernen, was Satelliten sind, wie sie aufgebaut
+                              sind, und welche Aufgaben sie im Weltraum
+                              übernehmen.
+                        </p>
+                        <img
+                              src="../assets/Intro_SatellitenBauKasten.png"
+                              alt="Hilfe beim Satelliten bauen"
+                        />
+                  </div>
+                  <div class="flexbox flexbox-2">
+                        <img
+                              src="../assets/Intro_Stefan_Bau.png"
+                              alt="Zwei Satellit-Baukasten"
+                        />
+                        <p class="text text-2">
+                              Der Satellitenbaukasten besteht aus 3D-gedruckten
+                              Satellitenmodellen in Originalgröße, die aus
+                              verschiedenen Satellitenkomponenten bestehen und
+                              selbstständig zusammengebaut werden können.
+                        </p>
+                        <img
+                              src="../assets/Intro_Junge_Bau.png"
+                              alt="Schüler baut Satelliten"
+                        />
+                  </div>
+            </div>
+      </div>
 </template>
 
 <script>
 export default {
-        name: "IntroScreen",
-
-}
+      name: "IntroScreen",
+};
 </script>
 
 <style scoped>
 .background {
-        background-image: url("../assets/Satellite_Background.jpg");
-        background-repeat: no-repeat;
-        background-size: cover;
-        padding: 0;
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
+      background-image: url("../assets/Satellite_Background.jpg");
+      background-repeat: no-repeat;
+      background-size: cover;
+      padding: 0;
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
 }
 
 .wrapper {
-        border: 1px solid rgb(243, 9, 9);
-        width: 100%;
-        max-width: var(--max-width-page);
+      border: 1px solid rgb(243, 9, 9);
+      width: 100%;
+      max-width: var(--max-width-page);
 }
 
 h1 {
-        font-size: var(--font-size-head);
-        margin: 100px 0 70px 0;
+      font-size: var(--font-size-head);
+      margin: 100px 0 70px 0;
+      text-align: center;
 }
 
 .flexbox {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 4%;
-        width: 100%;
+      border: 1px solid rgb(61, 177, 255);
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 4%;
+      width: 100%;
 }
 
 .flexbox-1 {
-        margin: 0px 0px 10px 0;
+      margin: 0px 0px 20px 0px;
 }
 
 .flexbox-2 {
-        padding: 100px 0px 20px 0;
+      padding: 0px 0px 0px 0px;
 }
 
-
 .text {
-        font-size: var(--font-size-normal);
-        max-width: 400px;
-        text-align: center;
-        text-shadow: 1px 1px 2px black;
-        /* maybe variable erstellen*/
+      font-size: var(--font-size-normal);
+      font-weight: var(--font-weight-normal);
+      max-width: 600px;
+      text-align: justify;
+      text-shadow: 1px 1px 2px black;
+      /* maybe variable erstellen*/
 }
 
 img {
-        min-width: 200px;
-        width: 30%;
-        /* maybe variable erstellen*/
-        max-width: 600px;
-        /* maybe variable erstellen*/
-        border-radius: var(--border-radius-photo);
+      min-width: 200px;
+      width: 25%;
+      /* maybe variable erstellen*/
+      max-width: 600px;
+      /* maybe variable erstellen*/
+      border-radius: var(--border-radius-photo);
 }
 
 /* ---------------------------Animation--------------------------- */
 
 @keyframes fadeInLeft {
-        0% {
-                opacity: 0;
-        }
+      0% {
+            opacity: 0;
+      }
 
-        100% {
-                opacity: 1;
-        }
+      100% {
+            opacity: 1;
+      }
 }
 
 h1 {
-        animation: fadeInLeft 2s ease-in-out forwards;
+      animation: fadeInLeft 2s ease-in-out forwards;
 }
 </style>
